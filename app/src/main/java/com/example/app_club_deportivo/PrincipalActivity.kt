d54483127b6f1a22e991c2592f80110237d0f0df
LeoDiaz225 <leodiaz225@outlook.com>
2026-05-06 23:24:44 -0300

    2da pantalla c/ img

---
+++ app/src/main/java/com/example/app_club_deportivo/PrincipalActivity.kt
@@ -0,0 +1,12 @@
+package com.example.app_club_deportivo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class PrincipalActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+}
+
