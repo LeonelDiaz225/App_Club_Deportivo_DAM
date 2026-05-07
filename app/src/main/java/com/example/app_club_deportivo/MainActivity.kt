8ba2bfc58b0937d0b1650473058a4d8859f6eaf6
LeoDiaz225 <leodiaz225@outlook.com>
2026-05-06 22:57:26 -0300

    test commit 1er pantalla

---
+++ app/src/main/java/com/example/app_club_deportivo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.app_club_deportivo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
