842a2b686915049698b7b5d5ad2316997ded505c
LeoDiaz225 <leodiaz225@outlook.com>
2026-05-07 23:08:18 -0300

    12va pantalla

---
+++ app/src/main/java/com/example/app_club_deportivo/GenerarComprobanteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.app_club_deportivo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class GenerarComprobanteActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generar_comprobante)
+    }
+}
+
