2c3f34135367986d15ae8c8036f039d0088a2a73
LeoDiaz225 <leodiaz225@outlook.com>
2026-05-07 21:57:57 -0300

    4ta pantalla

---
+++ app/src/main/java/com/example/app_club_deportivo/GestionDeNoSociosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.app_club_deportivo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class GestionDeNoSociosActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gestion_de_no_socios)
+    }
+}
