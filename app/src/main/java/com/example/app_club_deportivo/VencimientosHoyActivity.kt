ad9aafe0c692128322ea25a309580c4c39886c42
LeoDiaz225 <leodiaz225@outlook.com>
2026-05-07 23:00:27 -0300

    11va pantalla

---
+++ app/src/main/java/com/example/app_club_deportivo/VencimientosHoyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.app_club_deportivo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class VencimientosHoyActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vencimientos_hoy)
+    }
+}
